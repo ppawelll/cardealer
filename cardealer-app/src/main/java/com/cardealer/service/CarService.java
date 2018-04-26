2fbd842f115974bd90031ba3d666e76900b388fd
pawel <pawel_pszonka@epam.com>
2018-04-26 11:41:47 +0200

    US4 fixes

--- cardealer-app/src/main/java/com/cardealer/service/CarService.java
+++ cardealer-app/src/main/java/com/cardealer/service/CarService.java
@@ -4,7 +4,9 @@ import com.cardealer.dto.CarDTO;
 import com.cardealer.model.CarModel;
 import com.cardealer.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CarService {
 
     @Autowired
@@ -13,6 +15,6 @@ public class CarService {
     public void saveCar(CarDTO carDTO) {
 
         CarModel carModel = new CarModel(carDTO.getMake(), carDTO.getModel(), carDTO.getYear(), carDTO.getPrice());
-
+        carRepository.save(carModel);
     }
 }
